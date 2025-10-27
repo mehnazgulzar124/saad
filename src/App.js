aa63dc11a2af7842467fe62d2deebddb49155daa
mehnazgulzar124 <135806179+mehnazgulzar124@users.noreply.github.com>
2025-10-28 02:49:50 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,366 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  <>
+  
+
+  
+  <div class="container-fluid ">
+    <div class="row bg-header-img">
+   <nav class="nav-wrapper ">
+  <div class="logo">
+    <img src="/asserts/images/last-logo.png" alt="Logo" width="70" height="80" style={{marginTop:"5rem",marginLeft:"3rem"}}/>
+  </div>
+
+  <div class="menu-icon" id="menu-icon">☰</div>
+
+  <div class="nav-text" id="nav-text">
+    <div class="home">Home</div>
+    <div>Blog</div>
+    <div>Contact Us</div>
+    <div>Services</div>
+  </div>
+</nav>
+
+      <div class="col-lg-6 col-12 ps-5 d-flex flex-column justify-content-center first-row">
+        <div class="h-subheading">Delicious cafe</div>
+        <div class="h-heading">Sweet Treats,<div>Perfect Eats</div>
+        </div>
+
+        <div class="shop-now">
+          <button class="shop-btn ">Shop Now</button>
+          <a class="ankor-text ps-2" href="">Learn More</a>
+        </div>
+      </div>
     </div>
+
+    <div class="row r-p">
+
+      <div class="col-lg-12  d-flex justify-content-center py-5">
+        <div class="top-products-heading">Top Products</div>
+      </div>
+<div className='d-flex flex-wrap gap-4 justify-content-center'>
+      <div class="col-lg-3 col-6  ">
+        <div class="bg-product-img">
+          <img class="img-div pt-4" src="/asserts/images/1.png" alt=""/>
+          <div class="items">
+            <div class="d-flex justify-content-between  icon-div">
+              <div>$40</div>
+              <div><i class="bi bi-info-circle"></i></div>
+            </div>
+
+            <div class="d-flex justify-content-between pb-3 button-div">
+              <div class="whole">Whole Grain bread</div>
+              <button class="add-btn">Add</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-6 ">
+        <div class="bg-product-img">
+          <div class="img-div pt-4"><img src="/asserts/images/2.png" alt=""/></div>
+
+          <div class="items">
+            <div class="d-flex justify-content-between icon-div">
+              <div>$40</div>
+              <div><i class="bi bi-info-circle"></i></div>
+            </div>
+
+            <div class="d-flex justify-content-between pb-3  button-div button-div">
+              <div class="whole">Whole Grain bread</div>
+              <button class="add-btn">Add</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-6 ">
+        <div class="bg-product-img">
+          <img class="img-div pt-4 bg-cover" src="/asserts/images/3.png" alt="" />
+          <div class="items">
+            <div class="d-flex justify-content-between  icon-div">
+              <div>$40</div>
+              <div><i class="bi bi-info-circle"></i></div>
+            </div>
+
+            <div class="d-flex justify-content-between pb-3  button-div">
+              <div class="whole">Whole Grain bread</div>
+              <button class="add-btn">Add</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-6 ">
+        <div class="bg-product-img">
+          <img class="img-div pt-4" src="/asserts/images/4.png" alt=""/>
+          <div class="items">
+            <div class="d-flex justify-content-between  icon-div">
+              <div>$40</div>
+              <div><i class="bi bi-info-circle"></i></div>
+            </div>
+
+            <div class="d-flex justify-content-between pb-3 button-div">
+              <div class="whole">Whole Grain bread</div>
+              <button class="add-btn">Add</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+      <div class="col-lg-3 col-6 ">
+        <div class="bg-product-img">
+          <img class="img-div pt-4" src="/asserts/images/5.png" alt=""/>
+          <div class="items">
+            <div class="d-flex justify-content-between  icon-div">
+              <div>$40</div>
+              <div><i class="bi bi-info-circle"></i></div>
+            </div>
+
+            <div class="d-flex justify-content-between pb-3 button-div">
+              <div class="whole">Whole Grain bread</div>
+              <button class="add-btn">Add</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-6">
+        <div class="bg-product-img">
+          <img class="img-div pt-4" src="/asserts/images/6.png" alt="" />
+          <div class="items">
+            <div class="d-flex justify-content-between  icon-div">
+              <div>$40</div>
+              <div><i class="bi bi-info-circle"></i></div>
+            </div>
+
+            <div class="d-flex justify-content-between pb-3 button-div">
+              <div class="whole">Whole Grain bread</div>
+              <button class="add-btn">Add</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+     
+
+
+   
+      </div>
+      </div>
+      <div className="row">
+        <div className="col">
+                      <div class="Explore">Summer Food</div>
+
+        </div>
+      </div>
+<div class="container-fluid p-0">
+      <div class="row">
+        <div class="col-12">
+          <div class="offer-page">
+            <div class="d-flex flex-column justify-content-center align-items-center ">
+              <div class="heading">20% Off Your <div>First Order</div>
+              </div>
+              <div class="text-2">Suspendisse ac rhoncus nisl,<div>eu tempor urna. Curabitur vel</div> bibenjgg.</div>
+              <button class="learn">Learn More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="d-flex flex-column align-items-center">
+            <div class="Explore">Explore More</div>
+            <div class="d-flex gap-5 menu pt-5">
+              <div class="d-flex flex-column">
+                <div>Cake</div>
+                <div class="line"></div>
+              </div>
+              <div>Muffins</div>
+              <div>Croissant</div>
+              <div>Bread</div>
+              <div>Tart</div>
+              <div>Favorite</div>
+            </div>
+            <div class="big-line"></div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+      <div class="row row-cols-1 row-cols-md-3 padd g-4 mb-4">
+        <div class="col-4">
+          <div class="card h-100">
+            <img src="/asserts/images/7.png" class="card-img-top" alt="..."/>
+
+          </div>
+
+        </div>
+        <div class="col-4">
+          <div class="card h-100">
+            <img src="/asserts/images/8.png" class="card-img-top" alt="..."/>
+
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card h-100">
+            <img src="/asserts/images/9.png" class="card-img-top" alt="..."/>
+
+          </div>
+
+        </div>
+      </div>
+      <div class="row row-cols-1 row-cols-md-3 padd g-4">
+        <div class="col-4">
+          <div class="card h-100">
+            <img src="/asserts/images/10.png" class="card-img-top" alt="..."/>
+
+          </div>
+
+        </div>
+        <div class="col-4">
+          <div class="card h-100 m-0">
+            <img src="/asserts/images/11.png" class="card-img-top" alt="..."/>
+
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card h-100 m-0">
+            <img src="/asserts/images/12.png" class="card-img-top" alt="..."/>
+
+          </div>
+
+        </div>
+      </div>
+    
+      <div class="row">
+        <div class="col-12">
+          <div class=" wheat-bg">
+            <div class="d-flex flex-column justify-content-center align-items-center ">
+              <div class="heading-4">About us </div>
+              <div class="text-3">Suspendisse ac rhoncus nisl,<div>eu tempor urna. Curabitur vel</div> bibenjgg.</div>
+              <button class="learn">Read More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+          <div class="feature">
+            Featured Treats
+          </div>
+        </div>
+      </div>
+      <div class="row row-cols-1 row-cols-md-3 padd g-4">
+        <div class="col-4">
+          <div class="card h-100">
+            <img src="/asserts/images/13.png" class="card-img-top" alt="..."/>
+
+          </div>
+
+        </div>
+        <div class="col-4">
+          <div class="card h-100 m-0">
+            <img src="/asserts/images/14.png" class="card-img-top" alt="..."/>
+
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card h-100 m-0">
+            <img src="/asserts/images/15.png" class="card-img-top" alt="..."/>
+
+          </div>
+
+        </div>
+      </div>
+      <div class="row bg-colr">
+        <div class="col-4 ">
+          <div class=" text-sec">
+            <div>Puff Pastry </div>
+            <div>$8</div>
+          </div>
+        </div>
+        <div class="col-4 ">
+          <div class=" text-sec">
+            <div>Doughnuts </div>
+            <div>$8</div>
+          </div>
+        </div>
+        <div class="col-4 ">
+          <div class=" text-sec">
+            <div>Brownies </div>
+            <div>$8</div>
+          </div>
+        </div>
+      </div>
+
+  
+
+      <div class="row p-0">
+        <div class="col-12 last-padd">
+          <div class="last-bg-img d-flex flex-column ">
+            <div class="d-flex justify-content-between ">
+              <div class="l-bg">
+                <img src="./asserts/images/last-logo.png" alt="" class="img-fluid logo-img "/>
+              </div>
+
+              <div class="d-flex justify-content-center align-items-start icons-wrappers gap-3">
+                <div class="follow-us">Follow us</div>
+                <img src="/asserts/images/facebook.png" alt="" class="img-fluid" style={{width:"30px"}}/>
+                <img src="/asserts/images/p-img.png" alt="" class="img-fluid" style={{width:"30px"}}/>
+                <img src="/asserts/images/whatsapp.png" alt="" class="img-fluid" style={{width:"30px"}}/>
+                <img src="/asserts/images/instagram.png" alt="" class="img-fluid" style={{width:"30px"}}/>
+              </div>
+            </div>
+            <div class="peach-line"></div>
+               <div class="row last-row">
+        <div class="col-lg-4 col-12 r-1">
+        <img src="/asserts/images/l-img-1.png" alt="" class="l-last-img"/>
+        </div>
+         <div class="col-lg-4 col-12 r-2">
+        <img src="/asserts/images/l-img-2.png" alt=""/>
+        </div>
+         <div class="col-lg-4 col-12 r-3">
+        <img src="/asserts/images/l-img-3.png" alt="" class="l-right-img"/>
+        </div>
+      </div>
+      <div class="row last-footer-text r-4">
+        <div class="col-12 d-flex justify-content-center">
+          <div class="design">© 2024 Bake House. All rights reserved</div>
+        </div>
+      </div>
+          </div>
+          
+        </div>
+       
+      </div>
+    
+
+   </div>
+
+   </>
+
+
+
+
+   
   );
 }
 
